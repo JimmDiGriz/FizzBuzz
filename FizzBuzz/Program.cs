9d870a693a12f2ff2f227dfdf3f0a92f9aed32d4
Intel <jimmdigriz@yandex.ru>
2015-09-20 20:07:20 +0300

    Init commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = i.ToString();
+                if (i % 3 == 0) { output = "Fizz"; }
+                if (i % 5 == 0) { output = "Buzz"; }
+                if (i % 3 == 0 && i % 5 == 0) { output = "FizzBuzz"; }
+                Console.WriteLine(output);
+            }
+            Console.ReadKey();
+        }
+    }
+}
